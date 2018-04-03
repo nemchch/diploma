09332eae105228b39151ad1e352e202286f4df03
Anastasia Nemchunovich <nemchunovich7@gmail.com>
2018-04-03 21:32:59 +0300

    Small fix

--- src/main/java/TcpEchoServer.java
+++ src/main/java/TcpEchoServer.java
@@ -8,33 +8,29 @@ import java.net.Socket;
 public class TcpEchoServer {
 
     private ServerSocket serverSocket;
-    private int port = 3345;
-
+    private static final int PORT = 3345;
 
     private TcpEchoServer() {
         try {
-            setServer(new ServerSocket(port));
+            setServer(new ServerSocket(PORT));
         } catch (Exception ex) {
-            System.err.println("Could not listen on port: " + port);
+            System.err.println("Could not listen on port: " + PORT);
         }
     }
-
 
     public static void main(String[] args) {
         try {
             TcpEchoServer server = new TcpEchoServer();
             server.serve();
-
-
         } catch (Exception e) {
             System.err.println("Couldn't start server:\n" + e);
         }
     }
 
-    public void stop() {
+    private void stop() {
         try {
-
             serverSocket.close();
+            System.out.println("TCP Echo Server Stopped on port " + PORT);
         } catch (Exception e) {
             System.out.println("error in stop server socket " + e);
         }
@@ -44,42 +40,28 @@ public class TcpEchoServer {
 
         Socket clientSocket;
         try {
-            while (true) {
+            System.out.println("TCP Echo Server Started on port " + PORT + ".\nWaiting for connection.....");
+            clientSocket = serverSocket.accept();
+            System.out.println("Client Connection successful\nWaiting for input.....");
 
-                System.out.println("TCP Echo Server Started on port "
-                        + port
-                        + " . \nWaiting for connection.....");
-                clientSocket = serverSocket.accept();
-                System.out.println("Client Connection successful");
-                System.out.println("Waiting for input.....");
-
-                PrintWriter out = new PrintWriter(
-                        clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        clientSocket.getInputStream()));
-
-                String inputLine;
-                //just read 1 single line then auto close
-                while ((inputLine = in.readLine()) != null) {
-                    System.out.println("Server: " + inputLine);
-                    if (inputLine.equals("quit")) {
-                        out.close();
-                        clientSocket.close();
-                        in.close();
-                        break;
-                    }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Server: " + inputLine);
+                if (inputLine.equals("quit")) {
+                    out.close();
+                    clientSocket.close();
+                    in.close();
+                    break;
                 }
-                //whether to close after a single request.
-                // the interrupt is critical otherwise cannot easily shutdown
             }
-
         } catch (IOException e) {
-            System.out.println("Exception in echo server. "
-                    + "\nExpected when shutdown. {}" + e.getLocalizedMessage());
+            System.out.println("Exception in echo server.\nExpected when shutdown. {}" + e.getLocalizedMessage());
         } finally {
-            if (serverSocket != null && !serverSocket.isClosed())
-                serverSocket.close();
-
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                stop();
+            }
         }
     }
 
